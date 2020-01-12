a37df87fb9a1d824b90dd7119a9ca749f636f80d
Jeff Sexton <sexton.jeffp@gmail.com>
2020-01-12 18:58:58 +0000

    Updated joinArrays function with sorted merge of the strech challenge requirements

--- joinArrays.js
+++ joinArrays.js
@@ -7,9 +7,23 @@ function concat(array1, array2) {
   //return array1.concat(array2);
 }
 
+function merge(array1, array2){
+  array2.forEach(function(value , index) {
+    array1.splice(index, 0, value);
+  })
+  array1.sort();
+  return array1;
+}
 
-
+/* concat test cases
 console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
 console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
 console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
 console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+*/
+
+
+//merge test cases
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
