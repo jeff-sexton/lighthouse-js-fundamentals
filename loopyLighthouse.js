35369983024973ed164f352ff61b03c9faf9ed60
Jeff Sexton <sexton.jeffp@gmail.com>
2020-03-03 03:53:48 +0000

    Refactored looply lighthouse solution

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -12,7 +12,7 @@ for (let i = 100; i <= 200; i++) {
 }
 */
 
-function loopyLighthouse(range, multiples, words){
+/* function loopyLighthouse(range, multiples, words){
   for (let i = range[0]; i <= range[1]; i++) {
     if ((i % multiples[0] === 0) && (i % multiples[1] === 0)){
       console.log(words[0] + words[1]);
@@ -23,6 +23,26 @@ function loopyLighthouse(range, multiples, words){
     } else {
       console.log(i);
     }
+  }
+
+} */
+
+function loopyLighthouse(range, multiples, words){
+  for (let i = range[0]; i <= range[1]; i++) {
+    let result = '';
+    if (i % multiples[0] === 0) {
+      //console.log(words[0]);
+      result += words[0];
+    }
+    if (i % multiples[1] === 0) {
+      //console.log(words[1]);
+      result += words[1];
+    }
+    if (result === '') {
+      //console.log(i);
+      result = i;
+    }
+    console.log(result);
   }
 
 }
